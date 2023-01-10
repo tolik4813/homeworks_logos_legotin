112e6a8e09e4cf3fe572aadb09c843f12ddd2071
tolik4813 <afikusovich@gmail.com>
2023-01-11 01:21:02 +0200

    add JS folder and add JS_hw_1

---
+++ JS_Basics_HW/hw1/script.js
@@ -0,0 +1,24 @@
+// Task 1
+
+let city;
+city = "Київ";
+city = "Львів";
+let address = city;
+alert(city);
+
+// Task 2
+
+let n = 4;
+let result = n * n * n;
+console.log(result);
+
+// Task 3
+
+let pen = 4;
+let pencil = 6;
+let penPrice = 5.25;
+let pencilPrice = 3.50;
+let resultPrice = (pen * penPrice) + (pencil * pencilPrice);
+let resultAmount = (pen + pencil);
+document.write
+("загальна кількість = ",resultAmount," загальна сума = ",resultPrice);
