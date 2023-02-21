a04931b99c63bbce61910e5ef3e495db303aba96
tolik4813 <afikusovich@gmail.com>
2023-02-21 19:35:12 +0200

    add some fix

--- JS_Basics_HW/hw5/script.js
+++ JS_Basics_HW/hw5/script.js
@@ -50,7 +50,7 @@ switch (userNumber) {
 
 // Task 3
 function maxNumber() {
-    let max = 0;
+    let max = arguments[0];
     for (let i = 0; i <= arguments.length ; i++) {
         if (arguments[i] > max) {
             max = arguments[i];
